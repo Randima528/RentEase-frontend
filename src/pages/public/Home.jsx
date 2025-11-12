3dc40b0766629e2414eabd4c42ff235fb62d7c6e
Randima528 <randimanethmini528@gmail.com>
2025-11-13 02:59:19 +0530

    feat: restructure Home component layout and import statements for better organization

--- src/pages/public/Home.jsx
+++ src/pages/public/Home.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import Navbar from '../../components/common/Navbar';
+import Hero from '../../components/common/Hero';
+import FeaturedProperties from '../../components/common/FeaturedProperties';
+import StatsSection from '../../components/common/StatsSection';
+import CTASection from '../../components/common/CTASection';
+import Footer from '../../components/common/Footer';
 
 
 const Home = () => {
   return (
-    
+  <div className="min-h-screen bg-gradient-to-r from-[#667eea] to-[#764ba2]">
       <Navbar />
-      
+      <Hero />
+      <FeaturedProperties />
+      <StatsSection />
+      <CTASection />
+      <Footer /> 
+    </div>  
   );
 };
 
